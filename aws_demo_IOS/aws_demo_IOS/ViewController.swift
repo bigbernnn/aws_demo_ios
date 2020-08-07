71918e1bee9c873f2e05359ff8a4fb53307b5a6d
Big.burn <nicolasbernier@MacBook-Air-2.local>
2020-08-07 08:41:52 -0400

    Initialized XCode repo

---
+++ aws_demo_IOS/aws_demo_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aws_demo_IOS
+//
+//  Created by Nicolas Bernier on 2020-08-07.
+//  Copyright © 2020 nicbernien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
